ad7411f70c977d2aa0cedbece08bb8a23fb89e82
Daniel <danielbenicio123@gmail.com>
2021-12-13 21:07:48 -0300

    update commit

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -5,22 +5,35 @@ module.exports = {
     const db = await Database()
     const pass = req.body.password
     let roomId
+    let isRoom = true
+    while(isRoom){
+      // Gera o número da sala
+      for(var i = 0; i < 6; i++){
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+        roomId += Math.floor(Math.random() * 10).toString()
+      }
 
-    for(var i = 0; i < 6; i++){
-      i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-      roomId += Math.floor(Math.random() * 10).toString()
+      // Verificar se esse número já existe
+      const roomExistIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomExistIds.some(roomExistIds => roomExistIds === roomId) 
+      if(! isRoom){
+        // Insere a sala no banco
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
     }
-
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES (
-      ${parseInt(roomId)},
-      ${pass}
-    )`)
 
     await db.close()
 
     res.redirect(`/room/${roomId}`)
+  },
+  open(req, res){
+    const roomId = req.params.room
+    res.render("room", {roomId: roomId})
   }
 }
