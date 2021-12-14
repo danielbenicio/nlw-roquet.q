f1e6e63826de1ca7d671fcf58989d38b4a9da409
Daniel <danielbenicio123@gmail.com>
2021-12-14 13:38:13 -0300

    final project ✨

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -32,8 +32,25 @@ module.exports = {
 
     res.redirect(`/room/${roomId}`)
   },
-  open(req, res){
+  async open(req, res){
+    const db = await Database()
     const roomId = req.params.room
-    res.render("room", {roomId: roomId})
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+    const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+    let isNoQuestions
+
+    if(questions.length == 0 ){
+      if(questionsRead.length == 0){
+        isNoQuestions = true
+      } 
+    }
+
+    res.render("room", {roomId: roomId, questions: questions, questionsRead:questionsRead, isNoQuestions: isNoQuestions})
+  },
+
+  enter(req, res) {
+    const roomId = req.body.roomId
+
+    res.redirect(`/room/${roomId}`)
   }
 }
